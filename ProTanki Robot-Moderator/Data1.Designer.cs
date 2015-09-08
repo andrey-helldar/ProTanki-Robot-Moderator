3915d3cb3a2591cc0148e73850c7005f8d4460a6
Helldar <helldar@ai-rus.com>
2015-09-08 22:26:52 +0800

    Исправлены базовые настройки приложения

--- ProTanki Robot-Moderator/Data1.Designer.cs
+++ ProTanki Robot-Moderator/Data1.Designer.cs
@@ -37,7 +37,7 @@ namespace AIRUS_Bot_Moderator {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("ai_rus")]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
         public string Group {
             get {
                 return ((string)(this["Group"]));
